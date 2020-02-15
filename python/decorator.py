4ea938e633ae98625600b507602517f85dc87f85
orpoUser <othon.reyes@gmail.com>
2020-02-15 12:32:22 -0800

    Add an example about decorators

--- python/decorator.py
+++ python/decorator.py
@@ -60,3 +60,93 @@ print(p_decorated("Tata"))
 # we can even reassing the greeting function to have a more friendly way to use it
 greeting_fun = p_decorator(greeting_fun) # p_decorated is add_p_tags
 print(greeting_fun("Tata"))
+
+
+### Using the decorator
+def p_decorator2(fun):
+  def add_p_tag(name):
+    return "<p>{0}</p>".format(fun(name))
+  return add_p_tag
+
+@p_decorator2
+def hi(name):
+  return "Hello {0}".format(name)
+
+print(hi("Tata"))
+
+# Adding more decorators
+def b_decorator2(fun):
+  def add_p_tag(name):
+    return "<b>{0}</b>".format(fun(name))
+  return add_p_tag
+
+def p_decorator3(fun):
+  def add_p_tag(name):
+    return "<p>{0}</p>".format(fun(name))
+  return add_p_tag
+
+@b_decorator2
+@p_decorator3
+def hi2(name):
+  return "Hello {0}".format(name)
+
+print(hi2("Tata"))
+
+
+# Decorating a method
+def p_self_decorator(fun):
+  def func_wrapper(self):
+    return "<p>{0}</p>".format(fun(self))
+  return func_wrapper
+
+class Person:
+  def __init__(self):
+    self.name = "John"
+    self.last = "Doe"
+  
+  @p_self_decorator
+  def get_name(self):
+    return "{0} {1}".format(self.name, self.last)
+
+person = Person()
+print(person.get_name())
+
+## Making the decxorator more generic by passing args and kwargs
+def p_generic_decorator(fun):
+  def func_wrapper(*args, **kwargs):
+    return "<p2>{0}</p2>".format(fun(*args, **kwargs))
+  return func_wrapper
+
+class Person2:
+  def __init__(self):
+    self.name = "John"
+    self.last = "Doe"
+  
+  @p_generic_decorator
+  def get_name(self):
+    return "{0} {1}".format(self.name, self.last)
+
+person = Person2()
+print(person.get_name())
+
+# Pasing parameters to the decorators
+def tag(tag):
+  def tag_wrapper(fun):
+    def func_wrapper(*args, **kwargs):
+      return "<{0}>{1}</{0}>".format(tag,fun(*args, **kwargs))
+    return func_wrapper
+  return tag_wrapper
+
+class Person3:
+  def __init__(self):
+    self.name = "John"
+    self.last = "Doe"
+  
+  @tag("p")
+  @tag("b")
+  def get_name(self):
+    return "{0} {1}".format(self.name, self.last)
+
+person = Person3()
+print(person.get_name())
+
